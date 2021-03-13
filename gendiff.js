418a6456f8ea6118601c74bf3172f5725022ab6f
A. Rzhanitsyn <risdeep@gmail.com>
2021-03-13 22:01:01 +0500

    step 3.  add diff logic

--- gendiff.js
+++ gendiff.js
@@ -1,14 +1,35 @@
+#!/usr/bin/env node.
 const { program } = require('commander');
 const fs = require('fs');
 const path = require('path');
 const _ = require('lodash');
 
-const getDifferenceByKeyValue = (entriesA, entriesB) => {
-    const keysAll = _.uniq([...entriesA.map(([key]) => key), ...entriesB.map(([key]) => key)]);
-    
-    for (const key of keysAll) {
-      
+const getDifferenceByKeyValue = (objA, objB) => {
+  const keysA = _.sortBy(Object.keys(objA));
+  const keysB = _.sortBy(Object.keys(objB));
+
+  const keysAll = _.uniq([...keysA, ...keysB]);
+
+  const diff = [`{`];
+  for (const key of keysAll) {
+    if (_.has(objA, key)) {
+      if (_.has(objB, key)) {
+        if (objA[key] === objB[key]) {
+          diff.push(`  ${key}: ${objA[key]}`);
+        } else {
+          diff.push(`- ${key}: ${objA[key]}`);
+          diff.push(`+ ${key}: ${objB[key]}`);
+        }
+      } else {
+        diff.push(`- ${key}: ${objA[key]}`);
+      }
+    } else {
+      diff.push(`+ ${key}: ${objB[key]}`);
+    }
   }
+
+  diff.push('}');
+  return diff.join(' \n  ');
 };
 
 program
@@ -17,20 +38,13 @@ program
   .option('-f, --format <type>', 'output format', 'plain')
   .arguments('<filepath1> <filepath2>')
   .action((pathToFile1, pathToFile2) => {
-    console.log('path1 :>> ', pathToFile1);
-    console.log('path2 :>> ', pathToFile2);
-
     const file1 = fs.readFileSync(path.resolve(process.cwd(), pathToFile1));
     const file2 = fs.readFileSync(path.resolve(process.cwd(), pathToFile2));
 
     const file1Data = JSON.parse(file1);
     const file2Data = JSON.parse(file2);
 
-    const file1DataKeyValue = _.sortBy(Object.entries(file1Data), ([key, value]) => key);
-    const file2DataKeyValue = _.sortBy(Object.entries(file2Data), ([key, value]) => key);
-
-    console.log(getDifferenceByKeyValue(file1DataKeyValue));
-    console.log(getDifferenceByKeyValue(file2DataKeyValue));
+    console.log(getDifferenceByKeyValue(file1Data, file2Data));
   });
 
 program.parse();
