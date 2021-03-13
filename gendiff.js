3bea60721a62c7e81746025ec105fa13717368e4
A. Rzhanitsyn <risdeep@gmail.com>
2021-03-13 21:02:59 +0500

    step 2. add format arg, and filepaths args

--- gendiff.js
+++ gendiff.js
@@ -1,4 +1,13 @@
 const { program } = require('commander');
 
-program.version('0.0.1').description('Compares two configuration files and shows a difference.');
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format', 'plain')
+  .arguments('<filepath1> <filepath2>');
+//   .action((path1, path2) => {
+//     console.log('path1 :>> ', path1);
+//     console.log('path2 :>> ', path2);
+//   });
+
 program.parse();
