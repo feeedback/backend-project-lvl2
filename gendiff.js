0eb0eb6925368d6517d9702dd66955ea7d2d884f
A. Rzhanitsyn <risdeep@gmail.com>
2021-03-15 01:42:44 +0500

    add

--- gendiff.js
+++ gendiff.js
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
-import commander from 'commander';
+import { Command } from 'commander';
 import getDifferenceTwoFile from './src/generate_diff_file.js';
 
-commander
+const program = new Command();
+
+program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((pathToFile1, pathToFile2, options) => {
     console.log(getDifferenceTwoFile(pathToFile1, pathToFile2, options.format));
-  });
-
-commander.parse(process.argv);
+  })
+  .parse();
 
 export default getDifferenceTwoFile;
