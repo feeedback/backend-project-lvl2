edcd74edde497ce96e9a8ab4d1abfb0961779239
A. Rzhanitsyn <risdeep@gmail.com>
2021-03-15 00:59:44 +0500

    fix typo

--- gendiff.js
+++ gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { program } from 'commander';
-import getDifferenceTwoFile from './src/get_diff_two_files.js';
+import getDifferenceTwoFile from './src/generate_diff_file.js';
 
 program
   .version('0.0.1')
