17ce5899e7041ccb6d0556a82d3c9c5efdef3564
A. Rzhanitsyn <risdeep@gmail.com>
2021-03-15 20:41:47 +0500

    fix pacakge.json main

--- gendiff.js
+++ gendiff.js
@@ -1,17 +1,14 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
+
+import program from 'commander';
 import getDifferenceTwoFile from './src/generate_diff_file.js';
 
-const program = new Command();
-
 program
-  .version('0.0.1')
+  .version('1.0.1')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((pathToFile1, pathToFile2, options) => {
-    console.log(getDifferenceTwoFile(pathToFile1, pathToFile2, options.format));
+  .action((pathToFile1, pathToFile2) => {
+    console.log(getDifferenceTwoFile(pathToFile1, pathToFile2, program.format));
   })
   .parse();
-
-export default getDifferenceTwoFile;
