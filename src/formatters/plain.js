33f814cd93c11f1c79140c7ef888828ac2237ac7
A. Rzhanitsyn <risdeep@gmail.com>
2021-09-09 19:03:51 +0500

    eslint fix

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -7,26 +7,26 @@ const getValueStr = (value) => {
   return typeof value === 'string' ? `'${value}'` : value;
 };
 
-const createDiffFormatted = (diffAST, keysAcc = []) => Object.values(diffAST).reduce(
-  (acc, {
-    key, children, type, value,
-  }) => {
-    const keysPath = [...keysAcc, key];
-    if (children) {
-      return [...acc, ...createDiffFormatted(children, keysPath)];
-    }
+const createDiffFormatted = (diffAST, keysAcc = []) => Object.values(diffAST).reduce((acc, {
+  key, children, type, value,
+}) => {
+  const keysPath = [...keysAcc, key];
+  if (children) {
+    return [...acc, ...createDiffFormatted(children, keysPath)];
+  }
 
-    const keysPathStr = keysPath.join('.');
+  const keysPathStr = keysPath.join('.');
 
-    const mapTypeDiffToStr = {
-      no_changed: [],
-      added: [`Property '${keysPathStr}' was added with value: ${getValueStr(value)}`],
-      updated: [`Property '${keysPathStr}' was updated. From ${getValueStr(value[0])} to ${getValueStr(value[1])}`],
-      removed: [`Property '${keysPathStr}' was removed`],
-    };
+  const mapTypeDiffToStr = {
+    no_changed: [],
+    added: [`Property '${keysPathStr}' was added with value: ${getValueStr(value)}`],
+    updated: [
+      `Property '${keysPathStr}' was updated. From ${getValueStr(value[0])} to ${getValueStr(value[1])}`,
+    ],
+    removed: [`Property '${keysPathStr}' was removed`],
+  };
 
-    return [...acc, ...mapTypeDiffToStr[type]];
-  }, [],
-);
+  return [...acc, ...mapTypeDiffToStr[type]];
+}, []);
 
 export default (diffAST) => createDiffFormatted(diffAST).join('\n');
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -18,28 +18,27 @@ const createDiffFormatted = (diffAST, indent = 0, indentStep = 4) => {
   const nowIndent = indent + indentStep;
   const indent2 = ' '.repeat(indent + 2);
 
-  const diffBody = Object.values(diffAST)
-    .reduce((acc, {
-      key, children, type, value,
-    }) => {
-      if (children) {
-        return [...acc, `${indent2}  ${key}: ${createDiffFormatted(children, nowIndent)}`];
-      }
+  const diffBody = Object.values(diffAST).reduce((acc, {
+    key, children, type, value,
+  }) => {
+    if (children) {
+      return [...acc, `${indent2}  ${key}: ${createDiffFormatted(children, nowIndent)}`];
+    }
 
-      const mapTypeDiffToStr = {
-        no_changed: [`${indent2}  ${key}: ${stringify(value, nowIndent)}`],
-        added: [`${indent2}+ ${key}: ${stringify(value, nowIndent)}`],
-        updated: [
-          [
-            `${indent2}- ${key}: ${stringify(value[0], nowIndent)}`,
-            `${indent2}+ ${key}: ${stringify(value[1], nowIndent)}`,
-          ].join('\n'),
-        ],
-        removed: [`${indent2}- ${key}: ${stringify(value, nowIndent)}`],
-      };
+    const mapTypeDiffToStr = {
+      no_changed: [`${indent2}  ${key}: ${stringify(value, nowIndent)}`],
+      added: [`${indent2}+ ${key}: ${stringify(value, nowIndent)}`],
+      updated: [
+        [
+          `${indent2}- ${key}: ${stringify(value[0], nowIndent)}`,
+          `${indent2}+ ${key}: ${stringify(value[1], nowIndent)}`,
+        ].join('\n'),
+      ],
+      removed: [`${indent2}- ${key}: ${stringify(value, nowIndent)}`],
+    };
 
-      return [...acc, ...mapTypeDiffToStr[type]];
-    }, []);
+    return [...acc, ...mapTypeDiffToStr[type]];
+  }, []);
 
   const diff = ['{', diffBody.join('\n'), `${' '.repeat(indent)}}`];
   return diff.join('\n');
